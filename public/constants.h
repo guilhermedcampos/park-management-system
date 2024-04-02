a9889206ab9a374784eb7b8968faa9068fd59745
Guilherme Campos <110358692+guilhermedcampos@users.noreply.github.com>
2024-04-02 12:31:14 +0100

    Update constants.h

--- public/constants.h
+++ public/constants.h
@@ -5,11 +5,17 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+/*
+ * Numeric constants.
+ */
 #define MAX_PARKING_LOTS 20   // Maximum number of parking lots
 #define BUFSIZE 8192          // Maximum buffer size
 #define REGISTRATION_LENGTH 8 // Maximum registration length
 #define HASH_TABLE_SIZE 1000  // Hash table size
 
+/*
+ * Error messages.
+ */
 #define ERR_INVALID_DATE "invalid date.\n"
 #define ERR_NO_PARK "%s: no such parking.\n"
 #define ERR_INVALID_REG "%s: invalid licence plate.\n"
@@ -20,6 +26,14 @@
 #define ERR_TOO_MANY_PARKS "too many parks.\n"
 #define ERR_INVALID_ENTRY "%s: invalid vehicle entry.\n"
 #define ERR_INVALID_EXIT "%s: invalid vehicle exit.\n"
+
+/*
+ * Booleans.
+*/
+#define TRUE 1
+#define FALSE 0
+#define SUCCESS 1
+#define FAILURE 0
 
 
 #endif // CONSTANTS_H
